0df7ba4079ad0ec76f84f77bb15359dd533d95b2
guanyuanbin1313 <2925220648@qq.com>
2019-10-27 19:28:56 +0800

    123

---
+++ work/build/precache-manifest.02236323b63abc39e32292600e2ae6e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "635e0cce3595cfa9bbdb6c02eb823032",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a7548aad0ea38007ade",
+    "url": "/static/css/main.a4d79e86.chunk.css"
+  },
+  {
+    "revision": "4a854a68b36be2bfa428",
+    "url": "/static/js/2.0afc0a55.chunk.js"
+  },
+  {
+    "revision": "7a7548aad0ea38007ade",
+    "url": "/static/js/main.dc9d83c7.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  }
+]);
