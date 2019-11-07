a11387d17aeea349a07400ed6a310fc0cd3e5e9a
官源斌 <33643072+guanyuanbin1313@users.noreply.github.com>
2019-11-07 20:09:03 +0800

    Add files via upload

---
+++ work2/build/precache-manifest.ee1a619ddd3b7a7ffe8ae8c7df2bb08e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52b4d94d22da8d965c3143e3f0dcab3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "972854cc3264a0fb11c5",
+    "url": "/static/css/2.0b726e0e.chunk.css"
+  },
+  {
+    "revision": "040d1bc09bba9d2562c9",
+    "url": "/static/css/main.17d16ac8.chunk.css"
+  },
+  {
+    "revision": "972854cc3264a0fb11c5",
+    "url": "/static/js/2.cffa9f65.chunk.js"
+  },
+  {
+    "revision": "040d1bc09bba9d2562c9",
+    "url": "/static/js/main.d91dcfc7.chunk.js"
+  },
+  {
+    "revision": "628ea25a63cbc5b89a27",
+    "url": "/static/js/runtime-main.9f147cbc.js"
+  }
+]);
